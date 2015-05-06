fadcc0d6531eea918ca68b09b76ca304b4c38255
maechel <lars.maechel@gmail.com>
2015-05-06 20:55:59 +0200

    Added packages console and console.network

---
+++ src/console/network/Server.java
@@ -0,0 +1,56 @@
+package console.network;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Subject {
+	private int portNumber;
+	private ServerSocket server;
+
+	public Server(int portNumber) throws IOException {
+		this.portNumber = portNumber;
+	}
+
+	public void openClientConnection() {
+		Socket client = new Socket();
+		try {
+			server = new ServerSocket(portNumber);
+			System.out.println("Server Listening on port " + portNumber);
+
+			while (true) {
+				client = server.accept();
+				new ClientHandler(client).start();
+			}
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		} finally {
+			if (server != null) {
+				try {
+					server.close();
+					System.out.println("The server closed the connection");
+				} catch (IOException e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		}
+	}
+
+	@Override
+	public void register(Observer obs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unregister(Observer obs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyObservers(Observer obs) {
+		// TODO Auto-generated method stub
+		
+	}
+}
