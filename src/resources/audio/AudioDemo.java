11e1d5bfc1b98028ba26fe4c1935c6d9b43eee55
Maokei <rickard.joh@gmail.com>
2015-05-14 02:18:01 +0200

    audio fix

--- src/resources/audio/AudioDemo.java
+++ src/resources/audio/AudioDemo.java
@@ -1,6 +1,13 @@
 package resources.audio;
 
 import java.awt.FlowLayout;
+import java.io.File;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -12,11 +19,12 @@ public class AudioDemo extends JFrame {
 		SoundHolder.initSounds();
 		JButton explosion = new JButton("Explosion1");
 		explosion.addActionListener(ae -> { play(); });
-		add(explosion, FlowLayout.CENTER);
-
+		this.add(explosion);
+		this.setSize(100, 100);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setVisible(true);
+		this.pack();
 	}
 	
 	public void play() {
@@ -25,5 +33,11 @@ public class AudioDemo extends JFrame {
 	
 	public static void main(String[] args) {
 		new AudioDemo();
+		
+	//path test,
+	//Audio a = new Audio();
+	//a.loadAudio("src/res/audio/", "tilt.wav");
+	//a.playAudio();
+	
 	}
 }
