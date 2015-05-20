3f6efa479429dd5aad8cab092fb856e466f76b93
Maokei <rickard.joh@gmail.com>
2015-05-20 15:44:10 +0200

    message modified

--- src/battleship/network/Message.java
+++ src/battleship/network/Message.java
@@ -1,121 +1,27 @@
 package battleship.network;
-/**
- * @file Message.java
- * @date 2015-05-06
- * @author rickard(rijo1001), lars(lama1205)
- * */
 
-import battleship.player.Alignment;
-import battleship.player.ShipType;
-import java.io.Serializable;
-
-/**
- * @package console.network
- * @class Message
- * @brief Class describes a message from a user to to another or everyone. 
- * */
-public abstract class Message implements Serializable{
-	private static final long serialVersionUID = 7526476844562776147L;
-	private String sender;
-	private String receiver;
-	static final int LOGOUT = 0, MESSAGE = 1;
+public class Message {
 	private int type;
+	private String msg;
+	private String name;
+	public static final int LOGIN = 0, MESSAGE = 1, LOGOUT = 2;
 	
-	public Message(String sender, String receiver) {
-		this.sender = sender;
-		this.receiver = receiver;
-	}
-	
-	public String getMessageType() {
-		return "message";
+	public Message(int type, String name, String msg) {
+		this.type = type;
+		this.name = name;
+		this.msg = msg;
 	}
 	
 	public int getType() {
 		return type;
 	}
 	
-	public String getSender() {
-		return sender;
-	}
-	
-	public String getReceiver() {
-		return receiver;
-	}
-}
-
-
-
-/**
- * @package console.network
- * @class ChatMessag
- * @brief Class describes a ChatMessage from a user to to another or everyone. 
- * */
-class ChatMessage extends Message {
-	private String message;
-	
-	public ChatMessage(String sender, String receiver, String message) {
-		super(sender, receiver);
-		this.message = message;
-	}
-	
-	public ChatMessage(String sender, String message) {
-		super(sender, "");
-		this.message = message;
-	}
-	
-	
 	public String getMessage() {
-		return message;
+		return msg;
 	}
 	
-	@Override
-	public String toString() {
-		return "ChatMessage: " + getSender() + " : " + getReceiver();
-	}
-}
-
-
-/**
- * @package console.network
- * @class ShotMessage
- * @brief Class describes a battle message containing information about a shot fired
- * */
-class PositionShipMessage extends Message {
-	private int posX;
-	private int posY;
-	private ShipType type;
-	private Alignment shipAlignment;
-	
-	public PositionShipMessage(String sender, String receiver, int x, int y, Alignment a) {
-		super(sender, receiver);
-		posX = x;
-		posY = y;
-		shipAlignment = a;
+	public String getName() {
+		return name;
 	}
 	
-	public int getX() {return posX;}
-	public int getY() {return posY;}
-	public ShipType getShipType() {return type;}
-	public Alignment getAlignment() {return shipAlignment;}
-	
-	@Override
-	public String toString() {
-		return "PositionShipMessage: " + getSender() + " : " + getReceiver() + " : " + shipAlignment.toString() + " : " + type.toString();
-	}
-}
-
-/**
- * @package console.network
- * @class ValidMoveMessage
- * @brief Message class that describes the validation of a move
- * */
-class AnswerMessage extends Message {
-	private boolean answer;
-	
-	public AnswerMessage(String sender, String receiver, boolean v) {
-		super(sender, receiver);
-		answer = v;
-	}
-	
-	public boolean getAnswer() {return answer;}
 }
