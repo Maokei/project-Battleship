2e22a7153af441aa25c1a2dcb4f12427327f83a5
Maokei <rickard.joh@gmail.com>
2015-05-27 20:24:33 +0200

    lobby refiend

--- src/battleship/screen/Lobby.java
+++ src/battleship/screen/Lobby.java
@@ -1,31 +1,44 @@
+/**
+ *@file Lobby.java
+ *@author rickard, lars 
+ **/
 package battleship.screen;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.Action;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.UIManager;
 import javax.swing.WindowConstants;
 
+import battleship.game.Message;
+import battleship.game.Player;
 import battleship.network.Server;
 
 public class Lobby extends JFrame{
 	private static final long serialVersionUID = 611848419874984812L;
 	JPanel bottomButtons;
-	MessagePanel mesPanel;
+	ChatPanel chatPanel;
 	JList players;
 	JTextArea chat;
 	JTextField chatInput;
 	JButton chaBtn;
 	JButton playAi;
+	Player player;
 	
 	public Lobby() {
 		super("*** Battleship lobby ***");
@@ -49,18 +62,43 @@ public class Lobby extends JFrame{
 		chaBtn.setMargin(new Insets(25, 25, 25, 25));
 		playAi.setMargin(new Insets(25, 25, 25, 25));
 		//list
-		String test[] = {"bosse", "olle", "gustav"};
-		players = new JList(test);
-		players.setPreferredSize(new Dimension(300, 300));
+		
+		
 		JScrollPane pane = new JScrollPane(players);
-		mesPanel = new MessagePanel();
+		//Chat panel
+		chatPanel = new ChatPanel();
+		chatPanel.setPreferredSize(new Dimension(900, 400));
 		//add components
 		add(pane, BorderLayout.WEST);
-		add(mesPanel, BorderLayout.EAST);
+		add(chatPanel, BorderLayout.EAST);
 		add(bottomButtons, BorderLayout.SOUTH);
+		chaBtn.addActionListener(new ChallangeListener());
 		//
 		setSize(1200, 600);
 		setVisible(true);
+	}
+	
+	//listener class
+	class ChallangeListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(!players.isSelectionEmpty()) {
+				String sel = (String)players.getSelectedValue();
+				JOptionPane.showMessageDialog(new JFrame(), "Sending battle challange to opponent, " + sel);
+				player.sendMessage(new Message(Message.CHALLENGE, player.getName(), sel));
+				players.clearSelection();
+			}
+		}
+		
+	}
+	
+	private void setupPlayersList() {
+		String test[] = {"bosse", "olle", "gustav"};
+		players = new JList(test);
+		players.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		players.setSelectionBackground(Color.BLACK);
+		players.setPreferredSize(new Dimension(300, 300));
 	}
 	
 	public static void main(String[] args) {
