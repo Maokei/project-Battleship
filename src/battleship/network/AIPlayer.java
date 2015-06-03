d5b0ebebfb4136c3eed9ccc76997486e79134c56
Maokei <rickard.joh@gmail.com>
2015-06-03 14:25:33 +0200

    added documentation to AIPlayer.java

--- src/battleship/network/AIPlayer.java
+++ src/battleship/network/AIPlayer.java
@@ -1,3 +1,8 @@
+/**
+ * @file AIPlayer.java
+ * @date 2015-05-25
+ * @author rickard, lars
+ * */
 package battleship.network;
 
 import static battleship.game.Constants.*;
@@ -19,6 +24,12 @@ import battleship.gameboard.Grid;
 import battleship.ships.Alignment;
 import battleship.ships.Ship;
 
+/**
+ * AIPlayer
+ * @class AIPlayer
+ * @brief class describes an AI player.
+ * @implements BattlePlayer, NetworkOperations
+ * */
 public class AIPlayer implements BattlePlayer, NetworkOperations {
 	private String name = "AI";
 	private String opname = "";
@@ -39,6 +50,9 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 	private Alignment enemyShipAlignment;
 	private String reciever, server;
 
+	/**
+	 *Constructor 
+	 * */
 	public AIPlayer(String name) {
 		reciever = name;
 		server = "Server";
@@ -60,6 +74,13 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		//sendMessage(new Message(Message.DEPLOYED, "AI", ""));
 	}
 	*/
+	
+	/**
+	 * init
+	 * @function init
+	 * @brief initioation function for the ai player.
+	 * @return void
+	 * */
 	public void init() {
 		r = new Random();
 		shipPlacer = new RandomShipPlacer();
@@ -78,6 +99,11 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		listen();
 	}
 
+	/**
+	 * initEnemyGrid
+	 * @name initEnemeyGrid
+	 * @brief initiates a copy of the opponent's grid
+	 * */
 	private void initEnemyGrid() {
 		for (int row = 0; row < SIZE; row++) {
 			for (int col = 0; col < SIZE; col++) {
@@ -87,6 +113,12 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * displayPlayerGrid
+	 * @name displayPlayerGrid
+	 * @brief debug function to display grid in console
+	 * @return void
+	 * */
 	public void displayPlayerGrid() {
 		for (int row = 0; row < SIZE; row++) {
 			System.out.print("[");
@@ -97,6 +129,12 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * displayPlayerShips
+	 * @name displayPlayerShips
+	 * @brief debug function to display ships in console
+	 * @return void
+	 * */
 	public void displayPlayerShips() {
 		for (Ship ship : playerShips) {
 			System.out.print(ship.getType() + "[ ");
@@ -107,6 +145,12 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * displayEnemyGrid
+	 * @name displayEnemyGrid
+	 * @brief debug function to display grid in console
+	 * @return void
+	 * */
 	public void displayEnemyGrid() {
 		for (int row = 0; row < SIZE; row++) {
 			System.out.print("[");
@@ -117,6 +161,12 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * fire
+	 * @name fire
+	 * @brief ai player fires, based on valid targets in grid and sends out a fire message to the player.
+	 * @return void
+	 * */
 	public void fire() {
 		if (checkProbableTargets()) {
 			calculateNextTarget();
@@ -136,13 +186,25 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * checkProbableTargets
+	 * @name checkProbableTargets
+	 * @brief help function to evaluate if there are any valid targets.
+	 * @return boolean if there are/are not any valid targets
+	 * */
 	private boolean checkProbableTargets() {
 		if (probableTargets.size() > 0) {
 			return true;
 		}
 		return false;
 	}
-
+	
+	/**
+	 * calculateNextTarget
+	 * @name calculateNextTarget
+	 * @brief Calculate the next target for the ai player, using number of probable targets
+	 * @return void
+	 * */
 	private void calculateNextTarget() {
 		System.out.print("ProbableTargets [ ");
 		for (Grid grid : probableTargets) {
@@ -158,10 +220,18 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		int row = grid.getRow();
 		int col = grid.getCol();
 		System.out.println("FIRE " + row + ", " + col);
+		//send fire message
 		sendMessage(new Message(Message.MESSAGE, "AI", reciever, "FIRE "
 				+ Integer.toString(row) + " " + Integer.toString(col)));
 	}
 
+	/**
+	 * checkHit
+	 * @name checkHit
+	 * @brief Function checks if a position is hit or not.
+	 * @param int row integer grid and integer column in grid
+	 * @return return true if location already hit
+	 * */
 	public boolean checkHit(int row, int col) {
 		if (aiPlayerGrid[row][col] == occupied) {
 			return true;
@@ -169,6 +239,13 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		return false;
 	}
 
+	/**
+	 * registerFire
+	 * @name registerFire
+	 * @brief iterate through the grid and register a hit or a miss by the enemy
+	 * @param integer row in ai grid and integer column in ai grid 
+	 * @return void
+	 * */
 	public void registerFire(int row, int col) {
 		if (checkBounds(row, col)) {
 			if (checkHit(row, col)) {
@@ -183,6 +260,13 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	
+	/**
+	 * registerPlayerHit
+	 * @name registerPlayerHit
+	 * @brief iterate through the grid and register a hit or a miss by the player
+	 * @param integer row in grid and integer column in grid 
+	 * */
 	public void registerPlayerHit(int row, int col) {
 		if (checkBounds(row, col)) {
 			enemyGrid[row][col] = hit;
@@ -196,6 +280,13 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * comparePrevHits
+	 * @name comparePrevHits
+	 * @brief compare hits and decide if next target should be vertical or horizontal for ai.
+	 * @param Takes a grid char[][]
+	 * @return void
+	 * */
 	private void comparePrevHits(Grid grid) {
 		if (prevHit.getRow() == -1 && prevHit.getCol() == -1) {
 			prevHit = grid;
@@ -217,6 +308,12 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 	
+	/**
+	 * updateProbableTargets
+	 * @name updateProbableTargets
+	 * @brief update list of probable targets based of assumed players ship hits alignment
+	 * @return void
+	 * */
 	private void updateProbableTargets() {
 		if(enemyShipAlignment == Alignment.HORIZONTAL) {
 			for (Iterator<Grid> i = probableTargets.iterator(); i.hasNext();) {
@@ -235,6 +332,12 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * addNextPossibleTargets
+	 * @name addNextPossibleTargets
+	 * @brief Add the next possible target based of the given grid  
+	 * @param takes a char grid 10 * 10
+	 * */
 	private void addNextPossibleTargets(Grid grid) {
 		if (prevHit.getRow() == -1 || currHit.getRow() == -1) {
 			if (checkLeftGrid(grid))
@@ -264,6 +367,13 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * checkLeftGrid
+	 * @name checkLeftGrid
+	 * @brief 
+	 * @param Takes a player grid pos
+	 * @return Return true if position is hit or missed
+	 * */
 	private boolean checkLeftGrid(Grid grid) {
 		int row = grid.getRow();
 		int col = grid.getCol();
@@ -273,7 +383,13 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 		return false;
 	}
-
+	
+	/**
+	 * checkTopGrid
+	 * @name checkTopGrid
+	 * @param Takes a player grid, to be checked
+	 * @return Return true if position is hit or missed
+	 * */
 	private boolean checkTopGrid(Grid grid) {
 		int row = grid.getRow();
 		int col = grid.getCol();
@@ -284,6 +400,13 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		return false;
 	}
 
+	/**
+	 * checkRightGrid
+	 * @name checkRightGrid
+	 * @brief check grid position valid target to the right
+	 * @param takes a grid position to check right of.
+	 * @return true if location has be hit or missed
+	 * */
 	private boolean checkRightGrid(Grid grid) {
 		int row = grid.getRow();
 		int col = grid.getCol();
@@ -293,7 +416,14 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 		return false;
 	}
-
+	
+	/**
+	 * checkBottomGrid
+	 * @name checkBottomGrid
+	 * @brief check grid position valid target to the bottom(south)
+	 * @param takes a grid position to check right of.
+	 * @return true if location has be hit or missed
+	 * */
 	private boolean checkBottomGrid(Grid grid) {
 		int row = grid.getRow();
 		int col = grid.getCol();
@@ -304,6 +434,13 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		return false;
 	}
 
+	/**
+	 * registerEnemyHit
+	 * @name registerEnemyHit
+	 * @brief register hit on ship and decide if the ship is sunk or not. 
+	 * @param takes a ship, integer row and integer column.
+	 * @return void
+	 * */
 	public void registerEnemyHit(Ship ship, int row, int col) {
 		if (checkBounds(row, col)) {
 			sendMessage(new Message(Message.MESSAGE, "AI", reciever,"HIT "
@@ -311,7 +448,7 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 			aiPlayerGrid[row][col] = hit;
 			ship.hit();
 			if (!ship.isAlive()) {
-				sinkShip(ship);
+				sinkShip(ship); //call sink ship
 				--remainingShips;
 				if (remainingShips == 0) {
 					battleLost();
@@ -320,6 +457,14 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	
+	/**
+	 * sinkShip
+	 * @name sinkShip
+	 * @brief function sinks a ship and sends out appropriate message to opponent player
+	 * @param Takes a ship object
+	 * @return void
+	 * */
 	public void sinkShip(Ship ship) {
 		int row = ship.getStartPosition().getRow();
 		int col = ship.getStartPosition().getCol();
@@ -332,6 +477,13 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 
 	}
 
+	/**
+	 * registerPlayerMiss
+	 * @name registerPlayerMiss
+	 * @brief registers a miss on player duplicate grid
+	 * @param takes integer row and integer column grid coordinates.
+	 * @return void
+	 * */
 	public void registerPlayerMiss(int row, int col) {
 		if (checkBounds(row, col)) {
 			playerTurn = false;
@@ -341,6 +493,12 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * checkBounds 
+	 * @brief Help function to decide if the given position is valid within a grid.
+	 * @param integer row and integer column grid coordinate.
+	 * @return true if valid false if not valid.
+	 * */
 	private boolean checkBounds(int row, int col) {
 		if ((row >= 0 && row < SIZE) && (col >= 0 && col < SIZE)) {
 			return true;
@@ -350,6 +508,13 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * registerEnememyMiss 
+	 * @name registerEnemyMiss
+	 * @brief Function registers a miss by the player on the ai grid.
+	 * @param integer row and integer column grid coordinate.
+	 * @return void
+	 * */
 	public void registerEnemyMiss(int row, int col) {
 		aiPlayerGrid[row][col] = miss;
 		System.out.println("Enemy MISS " + row + ", " + col);
@@ -357,20 +522,42 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 				+ Integer.toString(row) + " " + Integer.toString(col)));
 	}
 
+	
+	/**
+	 * battleLost
+	 * @name battleLost
+	 * @brief Function sends out battleLost message to the opponent player.
+	 * @return void
+	 * */
 	public void battleLost() {
 		sendMessage(new Message(Message.LOST, "AI", reciever,""));
 	}
 
+	/**
+	 * sendMessage
+	 * @name sendMessage
+	 * @brief Help function to send out a generic message to the player using the client connection class. 
+	 * */
 	@Override
 	public void sendMessage(Message message) {
 		con.sendMessage(message);
 	}
 
+	/**
+	 * getName
+	 * @name getName
+	 * @return String ai opponent name
+	 * */
 	@Override
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * openConnection
+	 * @name openConnection
+	 * @return true if it was possible to open the connection else it's false.
+	 * */
 	@Override
 	public boolean openConnection() {
 		con = new ClientConnection("localhost", 10001);
@@ -379,22 +566,42 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		return true;
 	}
 
+	/**
+	 * listen
+	 * @name listen
+	 * @brief Start listening for messages. 
+	 * */
 	@Override
 	public void listen() {
 		if (openConnection())
 			new Thread(con).start();
 	}
 
+	/**
+	 * closeConnection
+	 * @name closeConnection
+	 * @brief Close the connection held clientConnection.
+	 * */
 	@Override
 	public void closeConnection() {
 		con.closeConnection();
 	}
 
+	/**
+	 * setOpponentDeployed
+	 * @name setOpponentDeployed
+	 * @brief set the state for if the player has deployed his ships.
+	 * */
 	@Override
 	public void setOpponentDeployed() {
 		opponentDeployed = true;
 	}
 
+	/**
+	 * setPlayerTurn
+	 * @name setPlayerTurn
+	 * @param boolean to set player turn state.
+	 * */
 	@Override
 	public void setPlayerTurn(boolean playerTurn) {
 		this.playerTurn = playerTurn;
@@ -403,6 +610,12 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * placeEnemeyShip
+	 * @name placeEnemyShip
+	 * @brief place an an enemy ship on the grid
+	 * @param Ship object, integer row and integer column coordinate.
+	 * */
 	@Override
 	public void placeEnemyShip(Ship ship, int row, int col) {
 		if (ship == null) {
@@ -419,6 +632,12 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * setShipPosition
+	 * @name setShipPosition
+	 * @brief Place a ship on the grid
+	 * @param Ship object, integer row and integer column coordinate
+	 * */
 	public void setShipPositions(Ship ship, int row, int col) {
 		int counter;
 		if (ship.getAlignment() == Alignment.HORIZONTAL) {
@@ -436,6 +655,11 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * checkEnemyDownProbableTargets
+	 * @name checkEnemyDownProbableTargets
+	 * @brief If there are still valid ship targets to aim for 
+	 * */
 	public void checkEnemyDownProbableTargets() {
 		if (!probableTargets.isEmpty()) {
 			for (Ship ship : shipsDown) {
@@ -510,6 +734,12 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 		}
 	}
 
+	/**
+	 * GameTimer
+	 * @class GameTimer
+	 * @brief And inner help class to help with timing of events.
+	 * @param integer seconds and integer amount of delay.
+	 * */
 	class GameTimer {
 		private Timer t;
 		private int seconds;
@@ -520,6 +750,11 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 			this.delay = delay;
 		}
 
+		/**
+		 * run
+		 * @name run
+		 * @briief start's thread
+		 * */
 		public void run() {
 			t = new Timer(delay, new ActionListener() {
 				@Override
@@ -531,6 +766,11 @@ public class AIPlayer implements BattlePlayer, NetworkOperations {
 			t.start();
 		}
 
+		/**
+		 * checkTime
+		 * @name checkTime
+		 * @brief if no time left stop.
+		 * */
 		private void checkTime() {
 			if (seconds <= 0) {
 				fire();
