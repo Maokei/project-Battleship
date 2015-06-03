0ea38b71085aa316796217e9b3896e27f42ce3de
Maokei <rickard.joh@gmail.com>
2015-06-03 15:35:09 +0200

    player op docs

--- src/battleship/game/PlayerOperations.java
+++ src/battleship/game/PlayerOperations.java
@@ -1,7 +1,15 @@
+/**
+ * @file PlayerOperations.java
+ * @author rickard, lars
+ * @date 2015-05-25
+ * */
 package battleship.game;
 
 import battleship.network.NetworkOperations;
 
+/**
+ * @interface PlayerOperations
+ * */
 public interface PlayerOperations {
 	public void init();
 	public void sendMessage(Message message);
